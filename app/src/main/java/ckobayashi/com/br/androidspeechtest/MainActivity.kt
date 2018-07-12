45041279adf1b9e71f4236bf9f16d18132e709b3
ckobayashi <ckobayashi@konkerlabs.com>
2018-07-12 09:12:36 -0300

    add text to speech support

--- app/src/main/java/ckobayashi/com/br/androidspeechtest/MainActivity.kt
+++ app/src/main/java/ckobayashi/com/br/androidspeechtest/MainActivity.kt
@@ -4,18 +4,20 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.*
-import kotlin.collections.RandomAccess
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
 
     private val REQ_CODE_SPEECH_INPUT = 100
     private var phrase: Phrase? = null
@@ -23,10 +25,14 @@ class MainActivity : AppCompatActivity() {
     private var quotesDAO: QuotesDAO? = null
     private var phrases: List<Phrase>? = null
 
+    private var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        tts = TextToSpeech(this, this)
 
         quotesDAO = QuotesDAO(this)
 
@@ -119,6 +125,29 @@ class MainActivity : AppCompatActivity() {
                     getString(R.string.speech_not_supported),
                     Toast.LENGTH_SHORT).show()
         }
+
+    }
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            val result = tts?.setLanguage(Locale("en", "US"))
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported")
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+
+    }
+
+    private fun speakOut() {
+
+        val text = phrase?.phrase
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null)
 
     }
 
